479ff4220271312971a26c77d81170c692999f06
Artem Dunaev <dunaev.aa@yandex.ru>
2021-05-25 16:16:50 +0500

    Fix component path

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import * as ActiveStorage from '@rails/activestorage';
 import 'material-design-lite/material.js';
 import 'channels';
 import WebpackerReact from 'webpacker-react';
-import TaskBoard from '../components/TaskBoard';
+import TaskBoard from 'components/TaskBoard';
 
 Rails.start();
 ActiveStorage.start();
