7587625f01505de484311d5b9a13617b371dd6ac
Artem Dunaev <dunaev.aa@yandex.ru>
2021-06-02 11:22:06 +0500

    Fix handleCardDragEnd

--- app/javascript/components/TaskBoard/TaskBoard.js
+++ app/javascript/components/TaskBoard/TaskBoard.js
@@ -86,11 +86,7 @@ const TaskBoard = () => {
       return null;
     }
 
-    return TasksRepository.update(task.id, {
-      task: {
-        stateEvent: transition.event,
-      },
-    })
+    return TasksRepository.update(task.id, { stateEvent: transition.event })
       .then(() => {
         loadColumnInitial(destination.toColumnId);
         loadColumnInitial(source.fromColumnId);
