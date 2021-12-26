2880a96a7f285c1d0bc93772250cea66d1d7915e
Artem Dunaev <artem.dunaev@dualbootpartners.com>
2021-12-26 15:00:02 +0500

    Add previews for mails

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,22 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def task_created
+    UserMailer.with(params).task_created
+  end
+
+  def task_edited
+    UserMailer.with(params).task_edited
+  end
+
+  def task_deleted
+    UserMailer.with(params).task_deleted
+  end
+
+  private
+
+  def params
+    user = User.first
+    task = Task.first
+    { user: user, task: task }
+  end
 end
